1c85dfe4ff1136c234e6489bb452baccae455a47
Pathologic <m@xim.name>
2022-05-25 13:52:29 +0300

    nl language

---
+++ assets/modules/reviews/lang/nl/reviews.inc.php
@@ -0,0 +1,22 @@
+<?php
+return [
+    'module.title'   => 'Reviews',
+    'module.close'   => 'Close module',
+    'pagetitle'      => 'Pagetitle',
+    'review'         => 'Review',
+    'name'           => 'Author',
+    'createdon'      => 'Created on',
+    'delete'         => 'Delete',
+    'sure_to_delete' => 'Are you sure to delete?',
+    'edit'           => 'Edit review',
+    'image'          => 'Image',
+    'save'           => 'Save',
+    'error'          => 'Error',
+    'close'          => 'Close',
+    'save_failed'    => 'Failed to save',
+    'response_error' => 'Failed to process server response',
+    'server_error'   => 'Server error: ',
+    'publish'        => 'Publish',
+    'rate'           => 'Rate'
+];
+
